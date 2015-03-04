99495ec4a86361a38088d6051757ad2dad916085
朱正晶 <zhuzhengjing@zhuzhengjingdeMac.local>
2015-03-04 10:30:26 +0800

    Initial Commit

---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by 朱正晶 on 15-3-4.
+//  Copyright (c) 2015年 China. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
